b1af7c534092b5249791ef6838a23f6b1c2482d5
yaashpanchal <166650386+yaashpanchal@users.noreply.github.com>
2024-04-11 16:53:15 +0530

    Create install_espeak.sh

---
+++ install_espeak.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Download and install espeak
+wget http://sourceforge.net/projects/espeak/files/espeak/espeak-1.48/setup_espeak-1.48.04.exe
+chmod +x setup_espeak-1.48.04.exe
+./setup_espeak-1.48.04.exe
